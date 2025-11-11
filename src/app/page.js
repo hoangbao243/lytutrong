ca48831fa7510ef995d1ec20b77ca2c87c6b7deb
hoangbao243 <killua7100@gmail.com>
2025-11-11 11:00:14 +0700

    third

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,15 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Imagelibrary from "@/components/imageLibrary/Imagelibrary";
-import Leftmenu from "@/components/Leftmenu";
 import Main from "@/components/Main";
 import Menu from "@/components/Menu";
 import News from "@/components/news/News";
 import Rightmenu from "@/components/Rightmenu";
-import Slideshow from "@/components/slideshow/Slideshow";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-6xl flex-col items-center bg-white dark:bg-black">
-        <Header></Header>
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Header></Header>
+      <main className="flex min-h-screen w-full max-w-6xl flex-col items-start bg-white dark:bg-black">
         <Menu></Menu>
         <section className="flex w-full p-2 h-1/3">
           <div className="flex flex-col w-3/4">
@@ -25,6 +21,7 @@ export default function Home() {
           </div>
         </section>
       </main>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/news/school/Schoolnews.jsx
+++ src/components/news/school/Schoolnews.jsx
@@ -1,69 +1,46 @@
+import Link from "next/link";
 import React from "react";
 
-export default function Schoolnews() {
-  const newsData = [
-    {
-      id: 1,
-      image: "images/11.png",
-      title:
-        "TẬP HUẤN PHÒNG CHÁY CHỮA CHÁY & CỨU NẠN CỨU HỘ NĂM HỌC 2025 - 2026",
-      createDate: "01-01-2025",
-      description:
-        "TẬP HUẤN PHÒNG CHÁY CHỮA CHÁY & CỨU NẠN CỨU HỘ NĂM HỌC 2025 - 2026 TẬP HUẤN PHÒNG CHÁY CHỮA CHÁY & CỨU NẠN CỨU HỘ NĂM HỌC 2025 - 2026 TẬP HUẤN PHÒNG CHÁY CHỮA CHÁY & CỨU NẠN CỨU HỘ NĂM HỌC 2025 - 2026",
-      userId: 1,
-      category: 1,
-    },
-    {
-      id: 2,
-      image: "images/11.png",
-      title:
-        "Thông báo về việc đề nghị phê duyệt phương án giá dịch vụ năm học 2025-2026",
-      createDate: "01-02-2025",
-      userId: 1,
-      category: 1,
-    },
-    {
-      id: 3,
-      image: "images/11.png",
-      title:
-        "Kế hoạch thực hiện quy định công khai trong các hoạt động của cơ sở giáo dục, NH 2025-2026",
-      createDate: "01-03-2025",
-      userId: 1,
-      category: 1,
-    },
-    {
-      id: 4,
-      image: "images/11.png",
-      title:
-        "TRƯỜNG TIỂU HỌC LÝ TỰ TRỌNG HÂN HOAN CHÀO ĐÓN NĂM HỌC MỚI 2025-2026",
-      createDate: "01-04-2025",
-      userId: 1,
-      category: 1,
-    },
-  ];
+export default function Schoolnews(props) {
+  const Data = props.props || [];
 
   return (
-    <div class="w-full h-full flex flex-col justify-start gap-2 bg-neutral-50 shadow p-2">
-      <div class="flex gap-2">
+    <div className="w-full h-full flex flex-col justify-start gap-2 bg-neutral-50 shadow p-2">
+      <div className="flex justify-start items-center bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-yellow-400 text-2xl font-black uppercase">
+        {props.title}
+      </div>
+
+      <div className="flex gap-2">
         <div className="clearfix">
-          <img
-            src={newsData[0].image}
-            alt={newsData[0].title}
-            className="float-left w-40 h-28 object-cover rounded-lg mr-3 mb-2"
-          />
-          <h3 className="font-bold text-blue-700 hover:underline line-clamp-2 text-sm md:text-base">
-            {newsData[0].title}
-          </h3>
-          <p className="text-gray-700 line-clamp-4 text-sm">
-            {newsData[0].description}
-          </p>
+          <Link href={`/post/${Data[0]?.id}`}>
+            <img
+              src={Data[0]?.image}
+              alt={Data[0]?.title}
+              className="float-left w-40 h-28 object-cover rounded-lg mr-3 mb-2"
+            />
+            <h3 className="font-bold text-blue-700 hover:underline line-clamp-2 text-sm md:text-base">
+              {Data[0]?.title}
+            </h3>
+            <p className="text-gray-700 line-clamp-4 text-sm">
+              {Data[0]?.description}
+            </p>
+          </Link>
         </div>
       </div>
       <ul>
-        {newsData &&
-          newsData?.slice(1)?.map((item) => (
-            <li>
-              <h3 className="line-clamp-1">{item.title}</h3>
+        {Data &&
+          Data?.slice(1)?.map((item) => (
+            <li key={item?.id} className="flex">
+              <img
+                className="m-1"
+                src="images/icon/right-arrow.png"
+                alt="right-arrow"
+                width={12}
+                height={12}
+              />
+              <h3 className="line-clamp-1 hover:text-blue-500">
+                <Link href={`/post/${item?.id}`}>{item?.title}</Link>
+              </h3>
             </li>
           ))}
       </ul>
