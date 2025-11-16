935f78c0e7437458596bde493d2dd14adfcce8ab
hoangbao243 <killua7100@gmail.com>
2025-11-16 23:34:50 +0700

    hot fix

--- src/components/announcement/Announcement.jsx
+++ src/components/announcement/Announcement.jsx
@@ -6,7 +6,7 @@ export default function Announcement(props) {
   
 
   return (
-    <div className="flex flex-col justify-start items-start w-full">
+    <div className="flex flex-col justify-start items-start w-full mt-1">
       <div className="flex h-fit w-full justify-start items-center border-b-2 border-blue-400 text-2xl uppercase">
         <div className="w-fit p-2 text-xl font-bold text-white bg-blue-400 rounded-t-xl">
           {props.title}
