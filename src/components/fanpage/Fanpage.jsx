1cc2cdf33c6d3bb932c16a06fae5ef8dae01344d
hoangbao243 <killua7100@gmail.com>
2025-11-17 12:57:10 +0700

    hot fix fanpage

--- src/components/fanpage/Fanpage.jsx
+++ src/components/fanpage/Fanpage.jsx
@@ -13,7 +13,7 @@ export default function Fanpage() {
     , []);
     
   return (
-    <div className="flex flex-col justify-start items-start h-full w-full lg:mt-1">
+    <div className="flex flex-col justify-start items-start h-fit w-full lg:mt-1">
       <div className="flex h-fit w-full justify-start items-center border-b-2 border-blue-400 text-2xl uppercase">
         <div className="w-fit p-2 text-xl font-bold text-white bg-blue-400 rounded-t-xl">
           Fanpage
